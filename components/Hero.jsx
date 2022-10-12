36e10aa83a9f9d3fdc228efb3abdd41bc9f4c99a
Umang Patel <umangpatel3105@gmail.com>
2022-10-12 19:03:01 +0530

    fea: linked supabase

---
+++ components/Hero.jsx
@@ -0,0 +1,10 @@
+import { Box, Button, Container } from "@chakra-ui/react";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <Container>
+      <Button display={'flex'} alignItems={'center'} justifyContent={'center'}>Explore</Button>
+    </Container>
+  );
+}
