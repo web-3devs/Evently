5c6cab77a1eafcf3ff4cf0cd8b7c414d75dfdd4c
Harsh <harshgajjar145@gmail.com>
2022-10-17 23:12:19 +0530

    fea: profile page drawer

--- components/Profilebar.jsx
+++ components/Profilebar.jsx
@@ -1,16 +1,36 @@
+import { SmallCloseIcon } from "@chakra-ui/icons";
 import {
   Avatar,
+  AvatarBadge,
   Box,
   Button,
+  Center,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
+  IconButton,
+  Input,
   Stack,
   Text,
+  Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function Profilebar() {
+  const [editProfile, seteditProfile] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const user = useSelector((state) => state.userData);
+  const drawer = useRef();
 
   return (
     <Flex
@@ -41,6 +61,11 @@ export default function Profilebar() {
           rounded={"sm"}
           size="md"
           cursor="pointer"
+          ref={drawer}
+          onClick={() => {
+            seteditProfile(true);
+            onOpen();
+          }}
         >
           Edit profile
         </Button>
@@ -52,10 +77,135 @@ export default function Profilebar() {
           rounded={"sm"}
           size="md"
           cursor="pointer"
+          ref={drawer}
+          onClick={() => {
+            seteditProfile(false);
+            onOpen();
+          }}
         >
           Add events
         </Button>
       </Stack>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={drawer}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton
+            bg={"transparent"}
+            _hover={{
+              border: "1px",
+              rounded: "sm",
+            }}
+            _focus={{
+              bg: "transparent",
+            }}
+          />
+          <DrawerHeader>
+            {editProfile ? "Edit Profile" : "Add Event"}
+          </DrawerHeader>
+          <DrawerBody>
+            {editProfile ? (
+              <>
+                <FormControl id="userName">
+                  <Stack direction={["column", "row"]} spacing={6}>
+                    <Center>
+                      <Avatar size="xl" src={user.currentUser?.image}>
+                        <AvatarBadge
+                          as={IconButton}
+                          size="sm"
+                          rounded="full"
+                          top="-10px"
+                          colorScheme="red"
+                          aria-label="remove Image"
+                          icon={<SmallCloseIcon />}
+                        />
+                      </Avatar>
+                    </Center>
+                    <Center w="full">
+                      <Button
+                        colorScheme={"white"}
+                        border={"1px"}
+                        rounded="sm"
+                        color={"black"}
+                        w="full"
+                      >
+                        Change icon
+                      </Button>
+                    </Center>
+                  </Stack>
+                </FormControl>
+                <Stack spacing={5} mt={5}>
+                  <FormControl isRequired>
+                    <Input
+                      placeholder="Name"
+                      _placeholder={{ color: "gray.500" }}
+                      type="text"
+                      border={"1px"}
+                      borderColor="black"
+                      rounded="sm"
+                      outline={"none"}
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <Input
+                      placeholder="Description"
+                      _placeholder={{ color: "gray.500" }}
+                      type="text"
+                      border={"1px"}
+                      borderColor="black"
+                      rounded="sm"
+                    />
+                  </FormControl>
+                </Stack>
+              </>
+            ) : (
+              <Stack spacing={5} mt={5}>
+                <Input
+                  placeholder="Name"
+                  _placeholder={{ color: "gray.500" }}
+                  type="text"
+                  border={"1px"}
+                  borderColor="black"
+                  rounded="sm"
+                />
+                <Textarea
+                  placeholder="Description"
+                  _placeholder={{ color: "gray.500" }}
+                  type="text"
+                  border={"1px"}
+                  borderColor="black"
+                  rounded="sm"
+                />
+              </Stack>
+            )}
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button
+              colorScheme={"white"}
+              border={"1px"}
+              rounded="sm"
+              color={"black"}
+              mr={3}
+              onClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              colorScheme={"white"}
+              border={"1px"}
+              rounded="sm"
+              color={"black"}
+            >
+              Save
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Flex>
   );
 }
