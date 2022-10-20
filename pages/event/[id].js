3ce3ff4432d361067c7fd46567b28387ac944185
Harsh <harshgajjar145@gmail.com>
2022-10-20 16:56:23 +0530

    fixes

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -1,133 +1,105 @@
 import {
-	Box,
-	Button,
-	Container,
-	Flex,
-	Heading,
-	HStack,
-	Img,
-	Text,
-} from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import convertDate, { getTime } from '../../utils/formatDate'
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Img,
+  Text,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { useSelector } from "react-redux";
+import convertDate, { getTime } from "../../utils/formatDate";
 function EventDetail() {
-	const router = useRouter()
-	const { id } = router.query
-	const currentEvent = useSelector((state) => state.allEvents)
-	const eventdata = currentEvent.allEvents?.events[parseInt(id)]
-	return (
-		<Container maxW={'container.xl'}>
-			<Box mt={[6, 12]}>
-				<Box h='72'>
-					<Img
-						src='vercel.svg'
-						w='full'
-						h='full'
-					/>
-				</Box>
-				<Flex
-					px='4'
-					py='12'
-					justifyContent={'space-between'}
-					flexWrap='wrap'
-				>
-					<Box w={'3xl'}>
-						<Heading>{eventdata?.name}</Heading>
-						<Text textAlign={'justify'}>
-							{eventdata?.description}
-						</Text>
-						<Button
-							my={6}
-							px={8}
-							colorScheme='white'
-							color={'black'}
-							border='1px'
-							boxShadow='6px 6px 0px black'
-							rounded={'sm'}
-							size='lg'
-							cursor='pointer'
-							_hover={{
-								bg: 'purple.600',
-							}}
-						>
-							Register now
-						</Button>
-					</Box>
-					<Box
-						w={'xs'}
-						bg='white'
-						ml={['unset']}
-						rounded='lg'
-						border='1px'
-						boxShadow='6px 6px 0px black'
-						p='4'
-						h={'max-content'}
-					>
-						<Box
-							py='2'
-							borderBottom={'1px'}
-						>
-							<Heading fontSize={'2xl'}>OverView</Heading>
-						</Box>
-						<HStack mt={4}>
-							<Box w='24'>
-								<Text
-									fontSize={'md'}
-									fontWeight='semibold'
-								>
-									Type
-								</Text>
-							</Box>
-							<Text fontSize={'md'}>Offline</Text>
-						</HStack>
-						<HStack mt={4}>
-							<Box w='24'>
-								<Text
-									fontSize={'md'}
-									fontWeight='semibold'
-								>
-									Date
-								</Text>
-							</Box>
-							<Text fontSize={'md'}>
-								{eventdata?.date_time
-									? convertDate(eventdata?.date_time)
-									: null}
-							</Text>
-						</HStack>
-						<HStack mt={4}>
-							<Box w='24'>
-								<Text
-									fontSize={'md'}
-									fontWeight='semibold'
-								>
-									Time
-								</Text>
-							</Box>
-							<Text fontSize={'md'}>
-								{eventdata?.date_time
-									? getTime(eventdata?.date_time)
-									: null}
-							</Text>
-						</HStack>
-						<HStack mt={4}>
-							<Box w='24'>
-								<Text
-									fontSize={'md'}
-									fontWeight='semibold'
-								>
-									Venue
-								</Text>
-							</Box>
-							<Text fontSize={'md'}>Something</Text>
-						</HStack>
-					</Box>
-				</Flex>
-			</Box>
-		</Container>
-	)
+  const router = useRouter();
+  const { id } = router.query;
+  const currentEvent = useSelector((state) => state.allEvents);
+  const eventdata = currentEvent.allEvents?.events[parseInt(id)];
+  return (
+    <Container maxW={"container.xl"}>
+      <Box mt={[6, 12]}>
+        <Box h="72">
+          <Img src={eventdata?.image} w="full" h="full" objectFit={"contain"} />
+        </Box>
+        <Flex px="4" py="12" justifyContent={"space-between"} flexWrap="wrap">
+          <Box w={"3xl"}>
+            <Heading>{eventdata?.name}</Heading>
+            <Text textAlign={"justify"}>{eventdata?.description}</Text>
+            <Button
+              my={6}
+              px={8}
+              colorScheme="white"
+              color={"black"}
+              border="1px"
+              boxShadow="6px 6px 0px black"
+              rounded={"sm"}
+              size="lg"
+              cursor="pointer"
+              _hover={{
+                bg: "purple.600",
+              }}
+            >
+              Register now
+            </Button>
+          </Box>
+          <Box
+            w={"xs"}
+            bg="white"
+            ml={["unset"]}
+            rounded="lg"
+            border="1px"
+            boxShadow="6px 6px 0px black"
+            p="4"
+            h={"max-content"}
+          >
+            <Box py="2" borderBottom={"1px"}>
+              <Heading fontSize={"2xl"}>OverView</Heading>
+            </Box>
+            <HStack mt={4}>
+              <Box w="24">
+                <Text fontSize={"md"} fontWeight="semibold">
+                  Type
+                </Text>
+              </Box>
+              <Text fontSize={"md"}>Offline</Text>
+            </HStack>
+            <HStack mt={4}>
+              <Box w="24">
+                <Text fontSize={"md"} fontWeight="semibold">
+                  Date
+                </Text>
+              </Box>
+              <Text fontSize={"md"}>
+                {eventdata?.date_time
+                  ? convertDate(eventdata?.date_time)
+                  : null}
+              </Text>
+            </HStack>
+            <HStack mt={4}>
+              <Box w="24">
+                <Text fontSize={"md"} fontWeight="semibold">
+                  Time
+                </Text>
+              </Box>
+              <Text fontSize={"md"}>
+                {eventdata?.date_time ? getTime(eventdata?.date_time) : null}
+              </Text>
+            </HStack>
+            <HStack mt={4}>
+              <Box w="24">
+                <Text fontSize={"md"} fontWeight="semibold">
+                  Venue
+                </Text>
+              </Box>
+              <Text fontSize={"md"}>Something</Text>
+            </HStack>
+          </Box>
+        </Flex>
+      </Box>
+    </Container>
+  );
 }
 
-export default EventDetail
+export default EventDetail;
--- pages/events.jsx
+++ pages/events.jsx
@@ -1,127 +1,108 @@
 import {
-	Box,
-	Container,
-	Flex,
-	Heading,
-	Skeleton,
-	SkeletonCircle,
-	SkeletonText,
-	Tab,
-	TabList,
-	TabPanel,
-	TabPanels,
-	Tabs,
-	useToast,
-} from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import EventCard from '../components/EventCard'
-import EventSkleton from '../components/EventSkleton'
-import { allEvents } from '../context/slices/alleventsSlice'
+  Container,
+  Flex,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import EventCard from "../components/EventCard";
+import EventSkleton from "../components/EventSkleton";
+import { allEvents } from "../context/slices/alleventsSlice";
 export default function Events() {
-	const [isLoading, setisLoading] = useState(true)
-	const alleventsdata = useSelector((state) => state.allEvents)
-	const dispatch = useDispatch()
-	useEffect(() => {
-		if (!alleventsdata) return
-		getAllEvents().then((res) => {
-			dispatch(allEvents(res))
-			setisLoading(false)
-		})
-	}, [])
-	console.log(alleventsdata)
+  const [isLoading, setisLoading] = useState(true);
+  const alleventsdata = useSelector((state) => state.allEvents);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!alleventsdata) return;
+    getAllEvents().then((res) => {
+      dispatch(allEvents(res));
+      setisLoading(false);
+    });
+  }, []);
+  console.log(alleventsdata);
 
-	const getAllEvents = async () => {
-		try {
-			const fetchAllEvents = await fetch('/api/getallevents')
-			const eventsdata = await fetchAllEvents.json()
-			console.log(eventsdata)
-			if (!fetchAllEvents.ok) return
-			return eventsdata
-		} catch (err) {
-			setisLoading(true)
-		}
-	}
+  const getAllEvents = async () => {
+    try {
+      const fetchAllEvents = await fetch("/api/getallevents");
+      const eventsdata = await fetchAllEvents.json();
+      console.log(eventsdata);
+      if (!fetchAllEvents.ok) return;
+      return eventsdata;
+    } catch (err) {
+      setisLoading(true);
+    }
+  };
 
-	return (
-		<Container
-			maxW={'container.xl'}
-			p={0}
-		>
-			<Tabs
-				isFitted
-				variant='unstyled'
-			>
-				<TabList
-					mt={12}
-					maxW={'3xl'}
-					mx={'auto'}
-				>
-					<Tab
-						border={'1px'}
-						_selected={{
-							color: 'white',
-							bg: 'purple.500',
-							border: 0,
-						}}
-						roundedTopLeft={'md'}
-						roundedBottomLeft={'md'}
-					>
-						Upcoming Events
-					</Tab>
-					<Tab
-						border={'1px'}
-						_selected={{
-							color: 'white',
-							bg: 'purple.500',
-							border: 0,
-						}}
-						roundedTopRight={'md'}
-						roundedBottomRight={'md'}
-					>
-						Previous Events
-					</Tab>
-				</TabList>
-				<TabPanels>
-					<TabPanel>
-						<Flex
-							flexWrap={'wrap'}
-							justifyContent='center'
-							pb={10}
-						>
-							{isLoading ? (
-								<>
-									<EventSkleton />
-									<EventSkleton />
-									<EventSkleton />
-									<EventSkleton />
-									<EventSkleton />
-								</>
-							) : (
-								<>
-									{alleventsdata.allEvents?.events?.map(
-										(item, index) => {
-											return (
-												<EventCard
-													index={index}
-													name={item.name}
-													desc={item.description}
-													image={item.image}
-													date={item.date_time}
-													posted={item.created_at}
-												/>
-											)
-										}
-									)}
-								</>
-							)}
-						</Flex>
-					</TabPanel>
-					<TabPanel>
-						<Heading>Previous</Heading>
-					</TabPanel>
-				</TabPanels>
-			</Tabs>
-		</Container>
-	)
+  return (
+    <Container maxW={"container.xl"} p={0}>
+      <Tabs isFitted variant="unstyled">
+        <TabList mt={12} maxW={"3xl"} mx={"auto"}>
+          <Tab
+            border={"1px"}
+            _selected={{
+              color: "white",
+              bg: "purple.500",
+              border: 0,
+            }}
+            roundedTopLeft={"md"}
+            roundedBottomLeft={"md"}
+          >
+            Upcoming Events
+          </Tab>
+          <Tab
+            border={"1px"}
+            _selected={{
+              color: "white",
+              bg: "purple.500",
+              border: 0,
+            }}
+            roundedTopRight={"md"}
+            roundedBottomRight={"md"}
+          >
+            Previous Events
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Flex flexWrap={"wrap"} justifyContent="center" pb={10}>
+              {isLoading ? (
+                <>
+                  <EventSkleton />
+                  <EventSkleton />
+                  <EventSkleton />
+                  <EventSkleton />
+                  <EventSkleton />
+                  <EventSkleton />
+                </>
+              ) : (
+                <>
+                  {alleventsdata.allEvents?.events?.map((item, index) => {
+                    return (
+                      <EventCard
+                        key={item.id}
+                        index={index}
+                        name={item.name}
+                        desc={item.description}
+                        image={item.image}
+                        date={item.date_time}
+                        posted={item.created_at}
+                      />
+                    );
+                  })}
+                </>
+              )}
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Heading>Previous</Heading>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Container>
+  );
 }
