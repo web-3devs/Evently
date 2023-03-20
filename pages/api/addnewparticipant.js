33462ec202e4e5a24e5efb46fa9a9de6f2ce8df2
Umang Patel <umangpatel3105@gmail.com>
2023-03-21 02:38:23 +0530

    fea: new participant query added

---
+++ pages/api/addnewparticipant.js
@@ -0,0 +1,32 @@
+import prisma from '../../lib/prisma'
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		try {
+			const { email, name,event_id, department,course,enrollment } = req.body
+			console.log(email)
+			
+			const participent_data = await prisma.newparticipants.create({
+				data: {
+					name: name,
+					email: email,
+                    event_id:event_id,
+                    enrollment:enrollment,
+                    department:department,
+                    course:course,
+					events: {
+						connect: {
+							id: event_id,
+						},
+					},
+				},
+			})
+			res.status(200).json({ participent_data })
+		} catch (error) {
+			console.log(error)
+			res.status(400).json({ error })
+		}
+	} else {
+		res.status(200).json({ message: 'Method not allowed' })
+	}
+}
--- pages/event/[id].jsx
+++ pages/event/[id].jsx
@@ -21,7 +21,8 @@ import {
   FormControl,
   FormLabel,
   Input,
-  ModalFooter
+  ModalFooter,
+  Select,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -47,19 +48,18 @@ export default function EventDetail() {
   const [isRegistered, setIsRegistered] = useState(false);
   const [isloading, setIsloading] = useState(false);
   const [isDone, setIsDone] = useState(false);
-  const [organizerName, setOrganizerName] = useState('');
+  const [organizerName, setOrganizerName] = useState("");
   const [organizerImg, setOrganizerImg] = useState(null);
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
-  const [data, setData] = useState(
-    {
-      name: "",
-      email: "",
-      department: "",
-      enrollmentno: "",
-      course: ""
-    }
-  );
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    department: "",
+    enrollmentno: "",
+    course: "",
+  });
+  console.log('the data is ', data);
   if (router.isFallback) {
     return <Container>Loading</Container>;
   }
@@ -70,15 +70,60 @@ export default function EventDetail() {
         setIsRegistered(true);
     }
   }
-
+  async function register() {
+    try {
+      const body = {
+        email: data?.email,
+        name: data?.name,
+        event_id: eventdata.id,
+        enrollment: data?.enrollmentno,
+        department: data?.department,
+        course: data?.course
+      };
+      const addparticipent = await fetch("/api/addnewpartcipent", {
+        method: "post",
+        headers: {
+          "Content-type": "application/json ",
+        },
+        body: JSON.stringify(body),
+      });
+      const p_data = await addparticipent.json();
+      if (addparticipent.status === 406) {
+        toast({
+          title: "You already registered for event",
+          status: "warning",
+          position: "top",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      if (addparticipent.ok) {
+        toast({
+          title: "Succesfully registered for event",
+          status: "success",
+          position: "top",
+          duration: 4000,
+          isClosable: true,
+        });
+    }
+    } catch (error) {
+      console.log(error.name);
+      toast({
+        title: "Something went wrong,Try again",
+        status: "error",
+        position: "top",
+        duration: 4000,
+        isClosable: true,
+      });
+    }
+  }
   function organizerInfo() {
     allUser?.map((user) => {
       if (user?.id == eventdata?.created_by) {
         setOrganizerName(user?.name);
         setOrganizerImg(user?.image);
       }
-
-    })
+    });
   }
   useEffect(() => {
     checkForRegistered();
@@ -110,13 +155,16 @@ export default function EventDetail() {
         name: user.currentUser?.name,
         event_id: eventdata.id,
       };
-      const addparticipent = await fetch("https://www.evently.club/api/addpartcipent", {
-        method: "post",
-        headers: {
-          "Content-type": "application/json ",
-        },
-        body: JSON.stringify(body),
-      });
+      const addparticipent = await fetch(
+        "https://www.evently.club/api/addpartcipent",
+        {
+          method: "post",
+          headers: {
+            "Content-type": "application/json ",
+          },
+          body: JSON.stringify(body),
+        }
+      );
       const p_data = await addparticipent.json();
       if (addparticipent.status === 406) {
         toast({
@@ -181,30 +229,70 @@ export default function EventDetail() {
   return (
     <Container maxW={"5xl"} p={0}>
       <Box mt={[6, 12]}>
-        <Box w={'full'} h='52' bg={"purple.400"} borderTopRadius={'md'}>
-          <Img src={eventdata?.image} w="full" h="full" objectFit={"contain"} display={['block', 'none']} />
+        <Box w={"full"} h="52" bg={"purple.400"} borderTopRadius={"md"}>
+          <Img
+            src={eventdata?.image}
+            w="full"
+            h="full"
+            objectFit={"contain"}
+            display={["block", "none"]}
+          />
         </Box>
         <Flex>
-          <Box h="xs" w="sm" mt={'-36'} ml={8} bgColor={'#bdb2ff'} borderRadius={4} display={['none', 'block']}>
-            <Img src={eventdata?.image} w="full" h="full" objectFit={"contain"} />
+          <Box
+            h="xs"
+            w="sm"
+            mt={"-36"}
+            ml={8}
+            bgColor={"#bdb2ff"}
+            borderRadius={4}
+            display={["none", "block"]}
+          >
+            <Img
+              src={eventdata?.image}
+              w="full"
+              h="full"
+              objectFit={"contain"}
+            />
           </Box>
           <Box mt={8} ml={[4, 8]}>
-            <Flex alignItems='center'>
-              <Img src={'/calendar.svg'} w={8} h={8} mr={2} />
-              <Text fontSize={'lg'}>{eventdata?.date_time ? convertDate(eventdata?.date_time) : null} , {eventdata?.date_time ? '10:00 AM' : null}</Text>
+            <Flex alignItems="center">
+              <Img src={"/calendar.svg"} w={8} h={8} mr={2} />
+              <Text fontSize={"lg"}>
+                {eventdata?.date_time
+                  ? convertDate(eventdata?.date_time)
+                  : null}{" "}
+                , {eventdata?.date_time ? "10:00 AM" : null}
+              </Text>
             </Flex>
-            <Flex mt={4} alignItems={'start'}>
-              <Img src={'/locate.svg'} w={8} h={8} mr={2} />
-              <Text fontSize={'lg'}>IT Seminar Hall, SVIT, Vasad</Text>
+            <Flex mt={4} alignItems={"start"}>
+              <Img src={"/locate.svg"} w={8} h={8} mr={2} />
+              <Text fontSize={"lg"}>IT Seminar Hall, SVIT, Vasad</Text>
             </Flex>
           </Box>
         </Flex>
-        <Flex px="4" py="12" justifyContent={"space-between"} flexDirection={['column', 'column', 'row']}>
-
-          <Box minW={['xs', 'md']} maxW={['xs', 'xl', 'lg']} >
-            <Text fontWeight={500} fontSize={'xl'} borderBottom={'4px'} borderColor={'purple.400'} w={'fit-content'}>Event Details</Text>
-            <Heading fontWeight={'bold'} fontSize={'5xl'} mt={2}>{eventdata?.name}</Heading>
-            <Text textAlign={"justify"} fontSize={['xl', '2xl']} my={2}>{eventdata?.description}</Text>
+        <Flex
+          px="4"
+          py="12"
+          justifyContent={"space-between"}
+          flexDirection={["column", "column", "row"]}
+        >
+          <Box minW={["xs", "md"]} maxW={["xs", "xl", "lg"]}>
+            <Text
+              fontWeight={500}
+              fontSize={"xl"}
+              borderBottom={"4px"}
+              borderColor={"purple.400"}
+              w={"fit-content"}
+            >
+              Event Details
+            </Text>
+            <Heading fontWeight={"bold"} fontSize={"5xl"} mt={2}>
+              {eventdata?.name}
+            </Heading>
+            <Text textAlign={"justify"} fontSize={["xl", "2xl"]} my={2}>
+              {eventdata?.description}
+            </Text>
             <Text fontSize={"md"}>Offline Event</Text>
 
             {isRegistered ? (
@@ -220,7 +308,7 @@ export default function EventDetail() {
                 size="lg"
                 disabled={isRegistered}
                 cursor="pointer"
-                bg={'purple.400'}
+                bg={"purple.400"}
                 onClick={() => {
                   registerForEvent();
                 }}
@@ -250,7 +338,7 @@ export default function EventDetail() {
                 _hover={{
                   bg: "purple.200",
                 }}
-                bg={'purple.400'}
+                bg={"purple.400"}
               >
                 {isloading ? (
                   <>
@@ -278,52 +366,70 @@ export default function EventDetail() {
               <ModalContent>
                 <ModalHeader>Register for the event</ModalHeader>
                 <ModalCloseButton />
-                <ModalBody pb={6} >
-                  <Flex flexDirection={'column'} justifyContent={'space-between'} gap={4}>
-                  <FormControl isRequired>
-                    <Input
-                      placeholder="Name"
-                      _placeholder={{ color: "gray.500" }}
-                      type="text"
-                      border={"1px"}
-                      name="name"
-                      borderColor="black"
-                      rounded="sm"
-                      outline={"none"}
-                      onChange={handleChange}
-                    />
-                  </FormControl>
+                <ModalBody pb={6}>
+                  <Flex
+                    flexDirection={"column"}
+                    justifyContent={"space-between"}
+                    gap={4}
+                  >
+                    <FormControl isRequired>
+                      <Input
+                        placeholder="Name"
+                        _placeholder={{ color: "gray.500" }}
+                        type="text"
+                        border={"1px"}
+                        name="name"
+                        borderColor="black"
+                        rounded="sm"
+                        outline={"none"}
+                        onChange={handleChange}
+                      />
+                    </FormControl>
 
-                  <FormControl isRequired>
-                    <Input
-                      placeholder="Email"
-                      _placeholder={{ color: "gray.500" }}
-                      type="email"
-                      border={"1px"}
-                      name="email"
-                      borderColor="black"
-                      rounded="sm"
-                      outline={"none"}
-                      onChange={handleChange}
-                    />
-                  </FormControl>
-                  <FormControl isRequired>
-                    <Input
-                      placeholder="Enrollment No."
-                      _placeholder={{ color: "gray.500" }}
-                      type="email"
-                      border={"1px"}
-                      name="enrollmentno"
-                      borderColor="black"
-                      rounded="sm"
-                      outline={"none"}
-                      onChange={handleChange}
-                    />
-                  </FormControl>
+                    <FormControl isRequired>
+                      <Input
+                        placeholder="Email"
+                        _placeholder={{ color: "gray.500" }}
+                        type="email"
+                        border={"1px"}
+                        name="email"
+                        borderColor="black"
+                        rounded="sm"
+                        outline={"none"}
+                        onChange={handleChange}
+                      />
+                    </FormControl>
+                    <FormControl isRequired>
+                      <Input
+                        placeholder="Enrollment No."
+                        _placeholder={{ color: "gray.500" }}
+                        type="email"
+                        border={"1px"}
+                        name="enrollmentno"
+                        borderColor="black"
+                        rounded="sm"
+                        outline={"none"}
+                        onChange={handleChange}
+                      />
+                    </FormControl>
+                    <FormControl isRequired>
+                      <Select name="course" placeholder="Course" borderRadius={0} onChange={handleChange} border={"1px"} borderColor={"black"} >
+                        <option value="Cyber Security">Cyber Security</option>
+                        <option value="Mobile Development">Mobile Development</option>
+                        <option value="Data Science">Data Science</option>
+                      </Select>
+                    </FormControl>
+                    <FormControl isRequired>
+                      <Select name="department" placeholder="Department" borderRadius={0} onChange={handleChange} border={"1px"} borderColor={"black"} >
+                        <option value="IT">IT</option>
+                        <option value="Computer">Computer</option>
+                        <option value="MCA">MCA</option>
+                      </Select>
+                    </FormControl>
                   </Flex>
                 </ModalBody>
                 <ModalFooter>
-                  <Button colorScheme='black' bg={'purple.500'} mr={3}>
+                  <Button colorScheme="black" bg={"purple.500"} mr={3} onClick={async()=>{await register()}} >
                     Register
                   </Button>
                   <Button onClick={onClose}>Cancel</Button>
@@ -331,32 +437,34 @@ export default function EventDetail() {
               </ModalContent>
             </Modal>
           </Box>
-          <Flex flexDirection={['column', 'row', 'column']} justifyContent={['center', 'space-between', 'center']}>
-
+          <Flex
+            flexDirection={["column", "row", "column"]}
+            justifyContent={["center", "space-between", "center"]}
+          >
             <Box
               w={["full", "xs"]}
               bg="white"
               ml={["unset"]}
               rounded="lg"
               border="1px"
-              borderColor={'purple.400'}
+              borderColor={"purple.400"}
               p="4"
               h={"max-content"}
-              textAlign={'left'}
+              textAlign={"left"}
             >
               <Box py="1">
-                <Heading fontSize={"2xl"} py={2}>Organizer</Heading>
-                <Divider borderColor={'purple.400'} />
+                <Heading fontSize={"2xl"} py={2}>
+                  Organizer
+                </Heading>
+                <Divider borderColor={"purple.400"} />
               </Box>
               <HStack>
                 <Box w="14">
-                  <Avatar
-                    size={"md"}
-                    src={organizerImg}
-                    name={organizerName}
-                  />
+                  <Avatar size={"md"} src={organizerImg} name={organizerName} />
                 </Box>
-                <Text fontSize={"xl"} fontWeight={500}>{organizerName}</Text>
+                <Text fontSize={"xl"} fontWeight={500}>
+                  {organizerName}
+                </Text>
               </HStack>
             </Box>
             <Box
@@ -366,26 +474,64 @@ export default function EventDetail() {
               ml={["unset"]}
               rounded="lg"
               border="1px"
-              borderColor={'purple.400'}
+              borderColor={"purple.400"}
               p="4"
               h={"max-content"}
-              textAlign={'left'}
+              textAlign={"left"}
             >
-              <Heading fontSize={"2xl"} py={2}>Share</Heading>
+              <Heading fontSize={"2xl"} py={2}>
+                Share
+              </Heading>
               <Divider />
               <Flex mt={4}>
-                <Flex height={10} width={10} border={'1px'} borderColor={'black'} boxShadow="3px 3px 0px black" bgColor={'purple.400'} justifyContent={'center'} alignItems={'center'} mr={4}>
+                <Flex
+                  height={10}
+                  width={10}
+                  border={"1px"}
+                  borderColor={"black"}
+                  boxShadow="3px 3px 0px black"
+                  bgColor={"purple.400"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  mr={4}
+                >
                   <a
                     rel="noopener noreferer"
                     target={"_blank"}
                     href={`https://twitter.com/compose/tweet/?text=I am attending ${eventdata?.name}, Don't forgot to register at Evetly\n Register now:https://evently-delta.vercel.app  `}
-                  ><Img src='/instagram.svg' height={6} width={6} /></a>
+                  >
+                    <Img src="/instagram.svg" height={6} width={6} />
+                  </a>
                 </Flex>
-                <Flex height={10} width={10} border={'1px'} borderColor={'black'} boxShadow="3px 3px 0px black" bgColor={'purple.400'} justifyContent={'center'} alignItems={'center'} mr={4}>
-                  <a><Img src='/linkedin.svg' height={6} width={6} /></a>
+                <Flex
+                  height={10}
+                  width={10}
+                  border={"1px"}
+                  borderColor={"black"}
+                  boxShadow="3px 3px 0px black"
+                  bgColor={"purple.400"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  mr={4}
+                >
+                  <a>
+                    <Img src="/linkedin.svg" height={6} width={6} />
+                  </a>
                 </Flex>
-                <Flex height={10} width={10} border={'1px'} borderColor={'black'} boxShadow="3px 3px 0px black" bgColor={'purple.400'} justifyContent={'center'} alignItems={'center'} mr={4}>
-                  <a><Img src='/whatsapp.svg' height={6} width={6} /></a>
+                <Flex
+                  height={10}
+                  width={10}
+                  border={"1px"}
+                  borderColor={"black"}
+                  boxShadow="3px 3px 0px black"
+                  bgColor={"purple.400"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  mr={4}
+                >
+                  <a>
+                    <Img src="/whatsapp.svg" height={6} width={6} />
+                  </a>
                 </Flex>
                 {/* <a><Img src='/linkedin.svg' height={44} width={44} /></a>
                 <a><Img src='/whatsapp.svg' height={44} width={44} /></a> */}
